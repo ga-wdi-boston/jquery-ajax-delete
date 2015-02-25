994927fcc86705b5f4e8b3f334349eda7b73793b
Tom Dyer <tdyer1@gmail.com>
2015-02-25 18:49:15 -0500

    Updated index_done and people_get_done files.

--- public/js/people_get_done.js
+++ public/js/people_get_done.js
@@ -1,19 +1,41 @@
 $(document).ready(function(){
 
+  // This is the callback handler for the Ajax GET
   var getPeople = function(data){
-    $('#container').html(data);
-  },
-      errorHandler = function(jqXHR,textStatus,errorThrown){
-        var msg = "Request Failed: "+ textStatus;
-        alert(msg);
-        console.log(msg);
-      };
+    // data will be the contents of the HTTP Response
+    // from the server.
 
+    // In this case will be the JSON that represents all the
+    // people that the server knows about.
+    // Build html for that person and inject into
+    // the DOM.
+    var personHTML = '';
+    data.forEach(function(person){
+      var personHTML = "<div id='" + person.id  + "'> <dt>Name</dt><dd>";
+      personHTML += person.name + "</dd>";
+      personHTML += "<dt>Occupation</dt><dd>" + person.occupation + "</dd>";
+      personHTML += "<dt>Company</dt><dd>" + person.company_name + "</dd>";
+      personHTML += "</div><hr><br>";
+      $("#container").append(personHTML);
+
+
+    });;
+  };
+
+  var errorHandler = function(jqXHR,textStatus,errorThrown){
+    var msg = "Request Failed: "+ textStatus;
+    alert(msg);
+    console.log(msg);
+  };
+
+  // JQuery method that will invoke an HTTP Request.
+
+  // Make a HTTP GET request to http://localhost:3333/people
   $.ajax({
     url: '/people',
-    dataType: 'html'
+    dataType: 'json'
+    // accepts a function that will act as a callback handler.
   })
     .done(getPeople)
-    .fail(errorHandler)
-    .always(function(){ console.log('Finished Ajax GET REQUEST'); });
+    .fail(errorHandler);
 });
