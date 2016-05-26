27ae3cd5c73501af16aab087d08dfa1e419946f7
berziiii <brian@offpistedesigns.com>
2016-05-26 08:25:29 -0400

    Solution for POST training including GET

--- scripts/book-create.sh
+++ scripts/book-create.sh
@@ -1,8 +1,15 @@
+#!/bin/sh
+
+# use like this:
+#
+#     TITLE="Harry Potter: Deathly Hallows" $AUTHOR="J.K. Rowling" ./scripts/books.sh
+#
+
 curl --include --request "POST" "http://localhost:3000/books" \
   --header "Content-Type: application/json" \
   --data $'{
     "book": {
-      "title": "The Moon is a Harsh Mistress",
-      "author": "Robert A. Heinlein"
+      "title": $TITLE,
+      "author": $AUTHOR
     }
   }'
