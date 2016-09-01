bf040fd60c5ad26fb6705744ffdfe21e5bbc86ff
payne-chris-r <06cpayne@gmail.com>
2016-08-31 23:39:55 -0400

    Removed a `.`
    Had the wrong file path. Fixed now.

--- assets/scripts/books/events.js
+++ assets/scripts/books/events.js
@@ -2,7 +2,7 @@
 
 // const getFormFields = require('../../../lib/get-form-fields');
 
-const api = require('../api');
+const api = require('./api');
 const ui = require('./ui');
 
 const onGetBooks = function (event) {
